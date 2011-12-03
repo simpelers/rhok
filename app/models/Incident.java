c3aa693e6ae3ba1d84d238f854c05742e1bce862
felixtrepanier <felixtrepanier@gmail.com>
2011-12-03 15:29:47 -0500

    edited incident and added a basic view for show

--- app/models/Incident.java
+++ app/models/Incident.java
@@ -8,16 +8,32 @@ import play.db.jpa.*;
 @Entity
 public class Incident extends Model {
 	
-	public String name;
+	public String incidentTitle;
+	public String description;
+	public long incidentDate;
+	public String incidentType; ///ENUM
+	public long duration;
+	public String location;
+	public String direction;
 	public long latitude;
 	public long longitude;
-	public long timestamp;
-	
-	public Incident(String name, long latitude, long longitude, long timestamp) {
+	public String User;
+	public Incident(String incidentTitle, String description,
+			long incidentDate, String incidentType, long duration,
+			String location, String direction, long latitude, long longitude,
+			String user) {
 		super();
-		this.name = name;
+		this.incidentTitle = incidentTitle;
+		this.description = description;
+		this.incidentDate = incidentDate;
+		this.incidentType = incidentType;
+		this.duration = duration;
+		this.location = location;
+		this.direction = direction;
 		this.latitude = latitude;
 		this.longitude = longitude;
-		this.timestamp = timestamp;
+		User = user;
 	}
+	
+	
 }
