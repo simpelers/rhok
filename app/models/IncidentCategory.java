679bfdfdb944faaa3c99b64a44c58093adb6fb57
Sébastien de Grandpre <sebastien.2.de-grandpre@hec.ca>
2011-12-03 16:01:27 -0500

    Adding IncidentCategory.java

---
+++ app/models/IncidentCategory.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.*;
+
+import org.hibernate.annotations.Entity;
+
+
+@Entity
+public class IncidentCategory {
+ 
+	public String name;
+	public long duration;
+	
+	public IncidentCategory(String name, long duration) {
+		super();
+		this.name = name;
+		this.duration = duration;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+
+}
